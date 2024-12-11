1b085968bed7cd2638a7a6c453522025d2403902
tepk2924 <chohan0925@gmail.com>
2024-12-11 19:01:45 +0900

    대회맵 반영 중

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-A = [3, 2, 3, 4]
-print(", ".join(map(str, A)))
+import os
+
+path = input(" : ")
+with open(path, "r") as f:
+    lines = f.readlines()
+
+filename = os.path.basename(path).replace(".csv", "_reversed.csv")
+dirname = os.path.dirname(path)
+
+with open(os.path.join(dirname, filename), "w") as f:
+    for line in lines[::-1]:
+        f.write(line)
